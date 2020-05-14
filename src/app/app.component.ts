0c8ec85bade414d5440bb189e729df17c76c0987
unknown <DarshanBaranwal@MDXN00110374.bku.db.de>
2020-05-14 16:50:05 +0200

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,88 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit{
+  title = 'CareForOld';
+  Items = [];
+  private myWindow;
+
+  ngOnInit() {
+    this.Items = [
+      {
+        dishName: 'BarbeQue',
+        origin: 'Pan India',
+        starrating: 4,
+        foodVoucher: 'Food Voucher',
+        offersList: ['Buy a gift voucher'],
+        imageSrc: 'https://d4t7t8y8xqo0t.cloudfront.net/resized/750X436/restaurant%2F661930%2Frestaurant020191212091542.jpg'
+      },
+      {
+        dishName: 'PaneerTikka',
+        origin: 'Uttar Pradesh',
+        starrating: 4,
+        foodVoucher: 'Food Voucher',
+        offersList: ['Buy a gift voucher', 'Buy Cold drink with free chips'],
+        imageSrc: 'https://d4t7t8y8xqo0t.cloudfront.net/resized/750X436/restaurant%2F661930%2Frestaurant020191212091542.jpg'
+      },
+      {
+        dishName: 'Soya Chap',
+        origin: 'Ghaziabad',
+        starrating: 4,
+        foodVoucher: 'Food Voucher',
+        offersList: ['Buy a gift voucher'],
+        imageSrc: 'https://d4t7t8y8xqo0t.cloudfront.net/resized/750X436/restaurant%2F661930%2Frestaurant020191212091542.jpg'
+      },
+      {
+        dishName: '',
+        origin: '',
+        starrating: 4,
+        foodVoucher: 'Food Voucher',
+        offersList: ['Buy a gift voucher'],
+        imageSrc: 'https://d4t7t8y8xqo0t.cloudfront.net/resized/750X436/restaurant%2F661930%2Frestaurant020191212091542.jpg'
+      },
+      {
+        dishName: 'Dahi k Bhalle',
+        origin: 'Gorakhpur',
+        starrating: 4,
+        foodVoucher: 'Food Voucher',
+        offersList: ['Buy a gift voucher'],
+        imageSrc: 'https://d4t7t8y8xqo0t.cloudfront.net/resized/750X436/restaurant%2F661930%2Frestaurant020191212091542.jpg'
+      },
+      {
+        dishName: 'Chicken Tikka',
+        origin: 'Chennai',
+        starrating: 4,
+        foodVoucher: 'Food Voucher',
+        offersList: ['Buy a gift voucher'],
+        imageSrc: 'https://d4t7t8y8xqo0t.cloudfront.net/resized/750X436/restaurant%2F661930%2Frestaurant020191212091542.jpg'
+      },
+      {
+        dishName: 'Chicken RARA',
+        origin: 'Jama Masjid',
+        starrating: 4,
+        foodVoucher: 'Food Voucher',
+        offersList: ['Buy a gift voucher'],
+        imageSrc: 'https://d4t7t8y8xqo0t.cloudfront.net/resized/750X436/restaurant%2F661930%2Frestaurant020191212091542.jpg'
+      },
+      {
+        dishName: 'CHickek Korma',
+        origin: 'Lucknow',
+        starrating: 4,
+        foodVoucher: 'Food Voucher',
+        offersList: ['Buy a gift voucher'],
+        imageSrc: 'https://d4t7t8y8xqo0t.cloudfront.net/resized/750X436/restaurant%2F661930%2Frestaurant020191212091542.jpg'
+      },
+    ];
+  }
+
+  openItem() {
+    var data = "<p>This is 'myWindow'</p>";
+    this.myWindow = window.open("data:text/html," + encodeURIComponent(data),
+      "_blank", "width=200,height=100");
+    this.myWindow.focus();
+  }
+}
